c332438cec0f352a2a75853e49bf3a07997532f0
ElliotXLeo <elliotgaramendi@gmail.com>
2020-07-08 20:27:38 -0500

    Conexión a base de datos MySQL Server

---
+++ LleveCasera/src/modelo/Conexion.java
@@ -0,0 +1,33 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    // Declaramos la conexion a mysql
+    private static Connection con;
+    // Declaramos los datos de conexion a la bd
+    private static final String driver="com.mysql.jdbc.Driver";
+    private static final String user="root";
+    private static final String pass="";
+    private static final String url="jdbc:mysql://localhost:3306/lleve_casera";
+    
+    public Connection Conectar(){
+        con=null;
+        try{
+            Class.forName(driver);
+            // Nos conectamos a la bd
+            con= (Connection) DriverManager.getConnection(url, user, pass);
+            // Si la conexion fue exitosa mostramos un mensaje de conexion exitosa
+            if (con!=null){
+                System.out.println("Conexion establecida");
+            }
+        }
+        // Si la conexion NO fue exitosa mostramos un mensaje de error
+        catch (ClassNotFoundException | SQLException e){
+            System.out.println("Problemas al conectar");
+        }
+        return con;
+    }
+}
